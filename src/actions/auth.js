4894631d63d67ca7777525d6aac42a9bfcb35564
dcalderonjimeno <dcalderonjimeno@gmail.com>
2020-08-14 17:12:12 +0200

    handle signup post request

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,17 +1,17 @@
 import axios from 'axios';
 
 import { ROOT_URL } from '../config'
-import { response } from 'express';
 
-export function SignUp(fields, sucess) {
+export function signUp(fields, success) {
     console.log(fields);
     return function(dispatch) {
-        // axios.post(`${ROOT_URL}/signUp`, fields)
-        //     .then(response => {
-        //         //do something with response
-        //     })
-        //     .catch(err => {
-        //         if(err) { console.log(err) }
-        //     })
+        axios.post(`${ROOT_URL}/signUp`, fields)
+            .then(response => {
+                console.log(response);
+                success()
+            })
+            .catch(err => {
+                if(err) { console.log(err) }
+            })
     }
 }
