e0ef083d370d95756a478d5b63cb65310022939c
dcalderonjimeno <dcalderonjimeno@gmail.com>
2020-08-21 14:10:17 +0200

    tab-nav grid

--- src/components/tabnav.js
+++ src/components/tabnav.js
@@ -5,7 +5,16 @@ class TabNav extends Component {
     render() {
         return (
             <div className='tab-nav'>
-            
+                <div className='tab-nav__tabs'>
+                {
+                this.props.tab.map((tab, index) => {
+                    return <a className='tab-nav__tab'>{tab.title}</a>   
+                })
+                }
+                </div>
+                <div>
+                    {/* request and newsletters goes here */}
+                </div>
             </div>
         )
     }
