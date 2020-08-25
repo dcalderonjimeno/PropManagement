13b578a7984d3fb445128dd19d9673fa77a4a737
dcalderonjimeno <dcalderonjimeno@gmail.com>
2020-08-25 18:37:52 +0200

    mapped on a newsletter JSON item to the latest newsletter component

--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -6,12 +6,21 @@ import NewsletterLatest from './newsletterLatest';
 
 class NewsletterGrid extends Component {
     render() {
+
+        const latest = {
+            _id: '115',
+            title: 'Happy Holiday Fam',
+            body: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.',
+            date: new Date(),
+            imageUrl: 'http://via.placeholder.com/960x258' 
+        }
+
         return (
             <div className='newsletter-grid'>
                 {/* add button */}
                 <NewsletterBox date={new Date()}/>
                 <NewsletterArchive/>
-                <NewsletterLatest/>
+                <NewsletterLatest {...latest}/>
             </div>
         )        
     }
--- src/components/newsletter/newsletterLatest.js
+++ src/components/newsletter/newsletterLatest.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 
 class NewsletterLatest extends Component {
     render () {
+        const { body, title, imageUrl } = this.props;
         return (
             <div className='newsletter-latest'>
-                <h1 className='newsletter-latest__title'>title goes here</h1>
-                <img className='newsletter-latest__image' src='http://via.placeholder.com/960x258'/>
+                <h1 className='newsletter-latest__title'>{title}</h1>
+                <img className='newsletter-latest__image' src={this.props.imageUrl}/>
                 <div className='newsletter-latest__body'>
-                    <p>lorem!8</p>
+                    <p>{body}</p>
                 </div>
             </div>
         )
