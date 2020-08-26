2c5d3016a57c31d2a82bef9b051d8d76aad199fe
dcalderonjimeno <dcalderonjimeno@gmail.com>
2020-08-26 12:23:53 +0200

    provided history prop to newsletter children components

--- src/components/newsletter/newsletterLatest.js
+++ src/components/newsletter/newsletterLatest.js
@@ -4,7 +4,7 @@ import Button from '../button';
 class NewsletterLatest extends Component {
 
     handleEdit = () => {
-        console.log('trying to handle Edit');
+        this.props.history.push('./newsletter/edit');
     }
 
     render () {
