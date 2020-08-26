c9bdc6fffa1a9c4090c92dbd460579d904f507b9
dcalderonjimeno <dcalderonjimeno@gmail.com>
2020-08-26 13:28:00 +0200

    built newsletter new component and placed sigininForm into newsletter new component

---
+++ src/components/newsletter/newsletterNew.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewNewsletter extends Component {
+    render() {
+        return (
+            <div className='new-newsletter'>
+                
+            </div>
+        )
+    }
+}
+
+export default NewNewsletter;
