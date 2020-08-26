20331c3b7259656ec50750bf3506a421b8339bb6
dcalderonjimeno <dcalderonjimeno@gmail.com>
2020-08-26 14:20:10 +0200

    built newsletterNewForm

--- src/components/newsletter/newsletterNew.js
+++ src/components/newsletter/newsletterNew.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
+import NewNewsletterForm from './newsletterNewForm';
+
 class NewNewsletter extends Component {
+
+    onSubmit = (fields) => {
+        
+    }
+
     render() {
         return (
             <div className='new-newsletter'>
-                
+                <NewNewsletterForm onSubmit={(event) => this.onSubmit(event)}/>
             </div>
         )
     }
