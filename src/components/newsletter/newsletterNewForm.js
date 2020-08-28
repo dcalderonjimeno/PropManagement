6b4447d627ac82413cce6417484d480917f2911b
dcalderonjimeno <dcalderonjimeno@gmail.com>
2020-08-28 13:36:32 +0200

    added additional button option to form-button component

--- src/components/newsletter/newsletterNewForm.js
+++ src/components/newsletter/newsletterNewForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 
 import { FormTitle } from '../formTitle';
-import { FormInput, FormButton } from '../formFields';
+import { FormInput, FormButton, FormTextArea } from '../formFields';
 import TextLink from '../textLink';
 
 class NewNewsletterForm extends Component {
@@ -12,15 +12,32 @@ class NewNewsletterForm extends Component {
 
         return (
             <form onSubmit={handleSubmit} className='new-newsletter-form'>
-                <FormTitle className='new-newsletter-form__title' text='Login'/>
-                <Field 
-                    className='new-newsletter-form__email' 
-                    placeholder='Enter Email'  
-                    name='email' 
-                    type='email'
-                    title='Email'
+                <FormTitle className='new-newsletter-form__title' text='New Newlsletter'/>
+                <Field
+                    className='new-newsletter-form__newsletter-title'
+                    placeholder='Service Request Title (Here)'
+                    name='title'
+                    type='text'
+                    title='Newsletter Title'
                     component={FormInput}
                 />
+                <Field
+                    className='new-newsletter-form__body'
+                    placeholder='Service Request Description (Here)'
+                    name='body'
+                    type='text'
+                    title='Body'
+                    component={FormTextArea}
+                />
+                <Field 
+                    className='sign-in-form__submit'
+                    small={true} 
+                    danger={true}  
+                    name='submit' 
+                    type='submit'
+                    title='Submit'
+                    component={FormButton}
+                />  
             </form>
         )
     }
