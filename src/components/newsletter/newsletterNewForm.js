b4103fe76818de15c36800eadee16830dd5b4403
dcalderonjimeno <dcalderonjimeno@gmail.com>
2020-09-02 19:24:54 +0200

    mapped newsletter edit data into form

--- src/components/newsletter/newsletterNewForm.js
+++ src/components/newsletter/newsletterNewForm.js
@@ -8,8 +8,16 @@ class NewNewsletterForm extends Component {
     render() {
 
         const { handleSubmit, formTitle, newsletterToEdit } = this.props;
+
+        var title = null;
+        var body = null;
+        var imageUrl = null;
         
-        const { title, body, imageUrl } = newsletterToEdit;
+        if(newsletterToEdit) {
+            title = newsletterToEdit.title;
+            body = newsletterToEdit.body;
+            imageUrl = newsletterToEdit.imageUrl;
+        }
 
         return (
             <form onSubmit={handleSubmit} className='new-newsletter-form'>
@@ -21,6 +29,7 @@ class NewNewsletterForm extends Component {
                     type='text'
                     title='Newsletter Title'
                     component={FormInput}
+                    editValue={title ? title : null}
                 />
                 <Field
                     className='new-newsletter-form__body'
@@ -29,6 +38,7 @@ class NewNewsletterForm extends Component {
                     type='text'
                     title='Body'
                     component={FormTextArea}
+                    editValue={body ? body : null}
                 />
                 <Field 
                     className='new-newsletter-form__submit'
@@ -55,6 +65,7 @@ class NewNewsletterForm extends Component {
                     type='file' 
                     title='Image'
                     component={FormImage}
+                    imageUrl={imageUrl}
                 />
             </form>
         )
