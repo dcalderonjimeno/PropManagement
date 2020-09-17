01b8f2cae7343648e32b251de165a384c6e1cfcb
dcalderonjimeno <dcalderonjimeno@gmail.com>
2020-09-17 13:04:12 +0200

    parsed date objects in newsletter components to remove date errors

--- src/components/newsletter/newsletterArchive.js
+++ src/components/newsletter/newsletterArchive.js
@@ -5,15 +5,16 @@ import { connect } from 'react-redux';
 import history from '../../history';
 
 function ArchiveItem({title, date, _id, callback }) {
+    const parsedDate = new Date(date);
     return (
         <div className='archive-item'>
             <a onClick={() => callback(_id)} className='archive-item__title'>{title}</a>
             <div className='archive-item__date'>
-                { date.getMonth() + 1 }
+                { parsedDate.getMonth() + 1 }
                 /
-                { date.getDate() }
+                { parsedDate.getDate() }
                 /
-                { date.getFullYear() - 2000 }
+                { parsedDate.getFullYear() - 2000 }
             </div>
         </div>
     )
@@ -37,7 +38,7 @@ class NewsletterArchive extends Component {
     }
 }
 
-function masStateToProps(state) {
+function mapStateToProps(state) {
     const { newsletters } = state.newsletters;
     return {
         newsletters
